f4ed8b1d9dd6fc029a6948616d959173c3d42a42
ntaranaw <nikita.taranaw@gmail.com>
2021-03-02 20:05:40 +0300

    refactor

--- src/App.js
+++ src/App.js
@@ -1,33 +1,66 @@
-import React from 'react'
-import { Root, Routes, addPrefetchExcludes } from 'react-static'
-//
-import { Link, Router } from 'components/Router'
-import Dynamic from 'containers/Dynamic'
+import React from "react";
+import { Root, Routes, addPrefetchExcludes } from "react-static";
 
-import './app.css'
+import "./app.css";
 
 // Any routes that start with 'dynamic' will be treated as non-static routes
-addPrefetchExcludes(['dynamic'])
+addPrefetchExcludes(["dynamic"]);
 
 function App() {
   return (
-    <Root>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/dynamic">Dynamic</Link>
-      </nav>
+    <div className="container">
+      <div className="title">Подарочные сертификаты</div>
       <div className="content">
-        <React.Suspense fallback={<em>Loading...</em>}>
-          <Router>
-            <Dynamic path="dynamic" />
-            <Routes path="*" />
-          </Router>
-        </React.Suspense>
+        <div className="content-link">
+          <a
+            href="https://www.turandot-palace.ru/prazdnichnye-meropriyatiya-6-7-8-marta/"
+            className="link"
+          >
+            <img src="https://www.turandot-palace.ru/wp-content/uploads/2021/02/veb-banner-karmen.png" />
+          </a>
+          <div className="info">
+            Праздничные мероприятия 6, 7, 8 марта
+            <button className="btn">Купить сертификат</button>
+          </div>
+        </div>
+        <div className="content-link">
+          <a
+            href="https://www.turandot-palace.ru/latino-vecherinka-28-fevralya-1900/"
+            className="link"
+          >
+            <img src="https://www.turandot-palace.ru/wp-content/uploads/2021/02/veb-banner-latino.jpg" />
+          </a>
+          <div className="info">
+            Латино вечеринка 28 февраля 19:00
+            <button className="btn">Купить сертификат</button>
+          </div>
+        </div>
+        <div className="content-link">
+          <a
+            href="https://www.turandot-palace.ru/branch-23-fevralya/"
+            className="link"
+          >
+            <img src="https://www.turandot-palace.ru/wp-content/uploads/2021/02/veb-banner-muzhskoj-branch.jpg" />
+          </a>
+          <div className="info">Бранч 23 февраля
+          <button className="btn">Купить сертификат</button>
+          </div>
+        </div>
+        <div className="content-link">
+          <a
+            href="https://www.turandot-palace.ru/prazdnichnye-meropriyatiya-6-7-8-marta/"
+            className="link"
+          >
+            <img src="https://www.turandot-palace.ru/wp-content/uploads/2021/02/veb-banner-karmen.png" />
+          </a>
+          <div className="info">
+            Праздничные мероприятия 6, 7, 8 марта
+            <button className="btn">Купить сертификат</button>
+          </div>
+        </div>
       </div>
-    </Root>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
