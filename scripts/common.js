3b2c34e1dfd9601044e4ba5ee3f385e58fb3579d
Arthur Zherka <arthurzherko@gmail.com>
2021-03-14 23:01:01 +0100

    add templates

---
+++ scripts/common.js
@@ -0,0 +1,30 @@
+window.addEventListener('load', (event) => {
+
+  // **** NAV MENU ****
+
+  let menuTimeoutIDs = []
+
+  const sublinks = document.querySelectorAll(".submenu")
+
+  sublinks.forEach((link) => {
+    link.addEventListener("mouseenter", (event) => {
+      menuTimeoutIDs.forEach(window.clearTimeout)
+      sublinks.forEach((link) => link.classList.remove("open"))
+      event.target.classList.add("open")
+    })
+
+    link.addEventListener("mouseleave", (event) => {
+      const isMenuLink = event.relatedTarget.classList.contains("menu-link")
+      
+      if (isMenuLink && event.relatedTarget.getAttribute('href')) {
+        return event.target.classList.remove("open")
+      }
+
+      const timeoutID = setTimeout(() => {
+        event.target.classList.remove("open")
+      }, 1000)
+
+      menuTimeoutIDs.push(timeoutID)
+    })
+  })
+})
---
+++ server.js
@@ -0,0 +1,50 @@
+const path = require('path')
+
+const socialLinks = require('./data/socialLinks')
+const menuLinks = require('./data/menuLinks')
+const certificates = require('./data/certificates.json')
+
+const data = {
+  socialLinks,
+  menuLinks
+}
+
+const fastify = require('fastify')({
+  logger: true
+})
+
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'public'),
+  prefix: '/public/'
+})
+
+fastify.register(require('point-of-view'), {
+  engine: {
+    pug: require('pug')
+  }
+})
+
+fastify.get('/certificate/:id', async ({ params }, reply) => {
+  const certificate = certificates.find(({ id }) => Number(params.id) === id)
+
+  if (certificate) {
+    return reply.view('/templates/certificate.pug', { ...data, certificate })
+  }
+})
+
+fastify.get('/congrats', async (req, reply) => reply.view('/templates/congrats.pug', data))
+
+fastify.get('/gratitude', async (req, reply) => reply.view('/templates/gratitude.pug', data))
+
+fastify.get('/', async (req, reply) => reply.view('/templates/home.pug', { ...data, certificates }))
+
+const start = async () => {
+  try {
+    await fastify.listen(3000, '0.0.0.0')
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+
+start()
