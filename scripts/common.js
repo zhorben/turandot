c29b7ee13ed31d630cd372e49bbffcb285c4c4a9
ntaranaw <nikita.taranaw@gmail.com>
2021-03-17 19:08:33 +0300

    menu-animation

--- scripts/common.js
+++ scripts/common.js
@@ -1,30 +1,35 @@
-window.addEventListener('load', (event) => {
-
+window.addEventListener("load", (event) => {
   // **** NAV MENU ****
 
-  let menuTimeoutIDs = []
+  let menuTimeoutIDs = [];
 
-  const sublinks = document.querySelectorAll(".submenu")
+  const sublinks = document.querySelectorAll(".submenu");
 
   sublinks.forEach((link) => {
     link.addEventListener("mouseenter", (event) => {
-      menuTimeoutIDs.forEach(window.clearTimeout)
-      sublinks.forEach((link) => link.classList.remove("open"))
-      event.target.classList.add("open")
-    })
+      menuTimeoutIDs.forEach(window.clearTimeout);
+      sublinks.forEach((link) => link.classList.remove("open"));
+      event.target.classList.add("open");
+    });
 
     link.addEventListener("mouseleave", (event) => {
-      const isMenuLink = event.relatedTarget.classList.contains("menu-link")
-      
-      if (isMenuLink && event.relatedTarget.getAttribute('href')) {
-        return event.target.classList.remove("open")
+      const isMenuLink = event.relatedTarget.classList.contains("menu-link");
+
+      if (isMenuLink && event.relatedTarget.getAttribute("href")) {
+        return event.target.classList.remove("open");
       }
 
       const timeoutID = setTimeout(() => {
-        event.target.classList.remove("open")
-      }, 1000)
+        event.target.classList.remove("open");
+      }, 1000);
 
-      menuTimeoutIDs.push(timeoutID)
-    })
-  })
-})
+      menuTimeoutIDs.push(timeoutID);
+    });
+  });
+
+  document.querySelector(".burger").addEventListener("click", (event) => {
+    document.body.classList.toggle("open");
+    document.querySelector(".burger").classList.toggle("is-active")
+  });
+});
+
